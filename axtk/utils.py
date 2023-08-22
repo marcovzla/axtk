7c1cf716e07a42fdefa01e7eb9c89502a399d8d2
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-08-22 12:03:02 -0700

    more utils

--- axtk/utils.py
+++ axtk/utils.py
@@ -1,6 +1,7 @@
+import os
+import sys
 from typing import Any
 from collections.abc import Iterator, Iterable
-import os
 
 
 def is_namedtuple(obj) -> bool:
@@ -11,6 +12,24 @@ def is_namedtuple(obj) -> bool:
 def is_pathlike(obj) -> bool:
     """Returns True if obj can be a Path."""
     return isinstance(obj, (str, os.PathLike))
+
+
+def is_in_notebook():
+    """Returns True if running in a Jupyter Notebook."""
+    try:
+        get_ipython = sys.modules['IPython'].get_ipython
+        return 'IPKernelApp' in get_ipython().config
+    except:
+        return False
+
+
+def is_in_colab() -> bool:
+    """Returns True if running in Google Colab."""
+    try:
+        import google.colab  # type: ignore
+        return True
+    except:
+        return False
 
 
 def drop_duplicates(xs: Iterable[Any]) -> list[Any]:
