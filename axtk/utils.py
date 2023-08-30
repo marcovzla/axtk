17b4db1f3a61096728f03c5a2f8865e8dce87f44
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-08-30 12:00:37 -0700

    docstrings

--- axtk/utils.py
+++ axtk/utils.py
@@ -32,18 +32,66 @@ def is_in_colab() -> bool:
         return False
 
 
-def drop_duplicates(xs: Iterable[Any]) -> list[Any]:
-    """Deduplicates elements in iterable, preserving their order."""
+def deduplicate_preserve_order(xs: Iterable[Any]) -> list[Any]:
+    """
+    Removes duplicate elements from an iterable while preserving their original order.
+
+    This function takes an iterable and returns a list containing the unique elements from
+    the iterable, maintaining their original order. Duplicate elements are removed,
+    and the order of the remaining elements is preserved.
+
+    Args:
+        iterable (Iterable[Any]): The input iterable containing elements to be deduplicated.
+
+    Returns:
+        list[Any]: A list containing the unique elements from the input iterable in their
+        original order.
+
+    Example:
+        >>> input_list = [3, 2, 1, 2, 3, 4, 5, 4, 6]
+        >>> deduplicated_list = deduplicate_preserve_order(input_list)
+        >>> deduplicated_list
+        [3, 2, 1, 4, 5, 6]
+    """
     return list(dict.fromkeys(xs))
 
 
-def flatten(
+def recursive_flatten(
         xs: Iterable[Any],
         *,
         flatten_strings: bool = False,
         flatten_bytes: bool = False,
 ) -> Iterator[Any]:
-    """Flattens iterable recursively."""
+    """
+    Recursively flattens an iterable, yielding its elements one by one.
+
+    This function takes an iterable and returns an iterator that yields its elements in
+    a flattened manner, recursively processing nested iterables. The `flatten_strings`
+    and `flatten_bytes` flags control whether strings and bytes-like objects should be
+    treated as leaf elements or recursively flattened.
+
+    Args:
+        iterable (Iterable[Any]): The input iterable to be recursively flattened.
+        flatten_strings (bool, optional): If True, strings will be recursively flattened.
+            If False, strings will be treated as leaf elements. Defaults to False.
+        flatten_bytes (bool, optional): If True, bytes-like objects will be recursively
+            flattened. If False, bytes-like objects will be treated as leaf elements.
+            Defaults to False.
+
+    Returns:
+        Iterator[Any]: An iterator that yields elements from the input iterable in a
+        flattened manner.
+
+    Example:
+        >>> input_list = [1, [2, [3, 4]], [5, 6]]
+        >>> flattened_iter = recursive_flatten(input_list)
+        >>> list(flattened_iter)
+        [1, 2, 3, 4, 5, 6]
+
+    Note:
+        - By default, strings and bytes-like objects are treated as leaf elements.
+          Use the `flatten_strings` and `flatten_bytes` flags to control their behavior.
+    """
     for x in xs:
         if isinstance(x, Iterable):
             if isinstance(x, str) and not flatten_strings:
@@ -51,6 +99,6 @@ def flatten(
             elif isinstance(x, (bytes, bytearray)) and not flatten_bytes:
                 yield x
             else:
-                yield from flatten(x)
+                yield from recursive_flatten(x)
         else:
             yield x
