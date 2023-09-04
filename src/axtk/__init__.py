ed70d5f25d9f75401c0da5260c7041f0b336b2b3
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-09-03 20:00:26 -0700

    org project

--- src/axtk/__init__.py
+++ src/axtk/__init__.py
@@ -1,9 +1,7 @@
-from axtk.version import __version__
 from axtk.utils import is_in_notebook, is_in_colab
 from axtk.torch_utils import set_seed, enable_full_determinism
 
 __all__ = [
-    '__version__',
     'is_in_notebook',
     'is_in_colab',
     'set_seed',
