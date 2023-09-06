dc22b08895cc4befc8af06a9d7d633576516b769
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-09-06 11:33:52 -0700

    updates

--- src/axtk/hooks.py
+++ src/axtk/hooks.py
@@ -28,19 +28,28 @@ class Hook:
     def __del__(self):
         self.unregister_hook()
 
+    @property
+    def is_registered(self) -> bool:
+        """Returns True if hook is currently registered."""
+        return False
+
     def register_hook(self):
-        """Method to be implemented by subclasses for registering the hook."""
-        raise NotImplementedError('Subclasses must implement register_hook method.')
+        """Register hook."""
+        if self.is_registered:
+            raise Exception('hook is already registered')
 
     def unregister_hook(self):
-        """Method to be implemented by subclasses for unregistering the hook."""
-        raise NotImplementedError('Subclasses must implement unregister_hook method.')
+        """Unregister hook."""
+        if not self.is_registered:
+            raise Exception('hook is not currently registered')
 
 
 class HookManager(Hook):
-    def __init__(self, hooks: Iterable[Hook]):
+    def __init__(self, hooks: Optional[Iterable[Hook]] = None):
         super().__init__()
-        self.hooks = list(hooks)
+        self._hooks: list[Hook] = []
+        self._registered: bool = False
+        self.hooks = hooks
 
     def __len__(self):
         return len(self.hooks)
@@ -48,13 +57,29 @@ class HookManager(Hook):
     def __iter__(self):
         return iter(self.hooks)
 
+    @property
+    def hooks(self) -> list[Hook]:
+        return self._hooks
+
+    @hooks.setter
+    def hooks(self, hs: Optional[Iterable[Hook]]):
+        if self.is_registered:
+            raise Exception('cannot swap hooks while manager is registered')
+        self._hooks = [] if hs is None else list(hs)
+
+    @property
+    def is_registered(self) -> bool:
+        return self._registered
+
     def register_hook(self):
-        """Register all hooks managed by this HookManager."""
+        super().register_hook()
+        self._registered = True
         for hook in self.hooks:
             hook.register_hook()
 
     def unregister_hook(self):
-        """Unregister all hooks managed by this HookManager."""
+        super().unregister_hook()
+        self._registered = False
         for hook in self.hooks:
             hook.unregister_hook()
 
@@ -64,7 +89,12 @@ class TorchHook(Hook):
         super().__init__()
         self.handle: Optional[RemovableHandle] = None
 
+    @property
+    def is_registered(self) -> bool:
+        return self.handle is not None
+
     def unregister_hook(self):
+        super().unregister_hook()
         self.handle.remove()
         self.handle = None
 
@@ -80,6 +110,7 @@ class TensorHook(TorchHook):
         self._hook_function = hook_function
 
     def register_hook(self):
+        super().register_hook()
         self.handle = self.tensor.register_hook(self.hook_function)
 
     def hook_function(self, tensor: Tensor) -> Optional[Tensor]:
@@ -101,6 +132,7 @@ class ModuleForwardHook(TorchHook):
         self.prepend = prepend
 
     def register_hook(self):
+        super().register_hook()
         self.handle = self.module.register_forward_hook(
             hook=self.hook_function,
             prepend=self.prepend,
@@ -132,6 +164,7 @@ class ModulePreForwardHook(TorchHook):
         self.prepend = prepend
 
     def register_hook(self):
+        super().register_hook()
         self.handle = self.module.register_forward_pre_hook(
             hook=self.hook_function,
             prepend=self.prepend,
@@ -162,6 +195,7 @@ class ModuleBackwardHook(TorchHook):
         self.prepend = prepend
 
     def register_hook(self):
+        super().register_hook()
         self.handle = self.module.register_full_backward_hook(
             hook=self.hook_function,
             prepend=self.prepend,
@@ -191,6 +225,7 @@ class ModulePreBackwardHook(TorchHook):
         self.prepend = prepend
 
     def register_hook(self):
+        super().register_hook()
         self.handle = self.module.register_full_backward_pre_hook(
             hook=self.hook_function,
             prepend=self.prepend,
