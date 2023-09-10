ce7b29b9d43eb268f5c3194bc6a83956e0383289
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-09-10 11:49:18 -0700

    docstrings

--- src/axtk/identifiers.py
+++ src/axtk/identifiers.py
@@ -3,10 +3,10 @@ import re
 
 def split_snakecase(identifier: str) -> list[str]:
     """
-    Split a snake case string into its components.
+    Split a snake_case string into its components.
     
     Args:
-        identifier (str): A string in snake case format.
+        identifier (str): A string in snake_case format.
         
     Returns:
         list[str]: List of components extracted from the identifier.
@@ -15,7 +15,7 @@ def split_snakecase(identifier: str) -> list[str]:
 
 def join_snakecase(parts: list[str], *, preserve_case: bool = False) -> str:
     """
-    Join a list of strings into a snake case identifier.
+    Join a list of strings into a snake_case identifier.
     
     Args:
         parts (list[str]): List of string components.
@@ -23,7 +23,7 @@ def join_snakecase(parts: list[str], *, preserve_case: bool = False) -> str:
             Otherwise, all parts are converted to lowercase. Default is False.
             
     Returns:
-        str: A string in snake case format.
+        str: A string in snake_case format.
     """
     if not preserve_case:
         parts = [p.lower() for p in parts]
@@ -31,10 +31,10 @@ def join_snakecase(parts: list[str], *, preserve_case: bool = False) -> str:
 
 def abbreviate_snakecase(identifier: str) -> str:
     """
-    Abbreviate a snake case string by taking the first character of each component.
+    Abbreviate a snake_case string by taking the first character of each component.
     
     Args:
-        identifier (str): A string in snake case format.
+        identifier (str): A string in snake_case format.
         
     Returns:
         str: Abbreviated string.
@@ -45,50 +45,176 @@ def abbreviate_snakecase(identifier: str) -> str:
     return ''.join(parts)
 
 
-def split_kebab_case(identifier: str) -> list[str]:
+def split_kebabcase(identifier: str) -> list[str]:
+    """
+    Split a kebab-case string into its components.
+    
+    Args:
+        identifier (str): A string in kebab-case format.
+        
+    Returns:
+        list[str]: List of components extracted from the identifier.
+    """
     return identifier.split('-')
 
-def join_kebab_case(parts: str, *, preserve_case: bool = False) -> list[str]:
+def join_kebabcase(parts: list[str], *, preserve_case: bool = False) -> str:
+    """
+    Join a list of strings into a kebab-case identifier.
+    
+    Args:
+        parts (list[str]): List of string components.
+        preserve_case (bool, optional): If set to True, the original case of parts is preserved.
+            Otherwise, all parts are converted to lowercase. Default is False.
+            
+    Returns:
+        str: A string in kebab-case format.
+    """
     if not preserve_case:
         parts = [p.lower() for p in parts]
     return '-'.join(parts)
 
-def abbreviate_kebab_case(identifier: str) -> str:
-    parts = split_kebab_case(identifier)
-    parts = [p[0].lower() for p in parts]
+def abbreviate_kebabcase(identifier: str) -> str:
+    """
+    Abbreviate a kebab-case string by taking the first character of each component.
+    
+    Args:
+        identifier (str): A string in kebab-case format.
+        
+    Returns:
+        str: Abbreviated string.
+    """
+    parts = split_kebabcase(identifier)
+    # Ensure that each part has at least one character
+    parts = [p[0].lower() if p else '' for p in parts]
     return ''.join(parts)
 
 
-def split_camel_case(identifier: str) -> list[str]:
-    matches = re.finditer(r'.+?(?:(?<=[0-9])(?=[A-Z])|(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
+def split_camelcase(identifier: str) -> list[str]:
+    """
+    Split a camelCase string into its components.
+    
+    Args:
+        identifier (str): A string in camelCase format.
+        
+    Returns:
+        list[str]: List of components extracted from the identifier.
+    """
+    matches = re.finditer(r'.+?(?:(?<=[a-zA-Z])(?=[0-9])|(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])|$)', identifier)
     return [m.group(0) for m in matches]
 
-def join_camel_case(parts: list[str], *, first_lowercase: bool = False) -> str:
+def join_camelcase(parts: list[str], *, first_lowercase: bool = False) -> str:
+    """
+    Join a list of strings into a camelCase identifier.
+    
+    Args:
+        parts (list[str]): List of string components.
+        first_lowercase (bool, optional): If set to True, the first part of the identifier is in lowercase.
+            Otherwise, every part starts with an uppercase letter. Default is False.
+            
+    Returns:
+        str: A string in camelCase format.
+    """
     parts = [p.capitalize() for p in parts]
     if first_lowercase:
         parts[0] = parts[0].lower()
     return ''.join(parts)
 
-def abbreviate_camel_case(identifier: str) -> str:
-    parts = split_camel_case(identifier)
-    parts = [p[0].lower() for p in parts]
+def abbreviate_camelcase(identifier: str) -> str:
+    """
+    Abbreviate a camelCase string by taking the first character of each component.
+    
+    Args:
+        identifier (str): A string in camelCase format.
+        
+    Returns:
+        str: Abbreviated string.
+    """
+    parts = split_camelcase(identifier)
+    # Ensure that each part has at least one character
+    parts = [p[0].lower() if p else '' for p in parts]
     return ''.join(parts)
 
 
-def snakecase_to_camel_case(identifier: str, *, first_lowercase: bool = False) -> str:
-    return join_camel_case(split_snakecase(identifier), first_lowercase=first_lowercase)
+def snakecase_to_camelcase(identifier: str, *, first_lowercase: bool = False) -> str:
+    """
+    Convert a snake_case identifier to camelCase format.
 
-def snakecase_to_kebab_case(identifier: str, *, preserve_case: bool = False) -> str:
-    return join_kebab_case(split_snakecase(identifier), preserve_case=preserve_case)
+    Args:
+        identifier (str): A string in snake_case format.
+        first_lowercase (bool, optional): If set to True, the resulting identifier starts with a lowercase letter.
+            Default is False.
 
-def kebab_case_to_camel_case(identifier: str, *, first_lowercase: bool = False) -> str:
-    return join_camel_case(split_kebab_case(identifier), first_lowercase=first_lowercase)
+    Returns:
+        str: A string in camelCase format.
+    """
+    return join_camelcase(split_snakecase(identifier), first_lowercase=first_lowercase)
 
-def kebab_case_to_snakecase(identifier: str, *, preserve_case: bool = False) -> str:
-    return join_snakecase(split_kebab_case(identifier), preserve_case=preserve_case)
+def snakecase_to_kebabcase(identifier: str, *, preserve_case: bool = False) -> str:
+    """
+    Convert a snake_case identifier to kebab-case format.
 
-def camel_case_to_snakecase(identifier: str, *, preserve_case: bool = False) -> str:
-    return join_snakecase(split_camel_case(identifier), preserve_case=preserve_case)
+    Args:
+        identifier (str): A string in snake_case format.
+        preserve_case (bool, optional): If set to True, the original case of identifier is preserved.
+            Default is False.
 
-def camel_case_to_kebab_case(identifier: str, *, preserve_case: bool = False) -> str:
-    return join_kebab_case(split_camel_case(identifier), preserve_case=preserve_case)
+    Returns:
+        str: A string in kebab-case format.
+    """
+    return join_kebabcase(split_snakecase(identifier), preserve_case=preserve_case)
+
+def kebabcase_to_camelcase(identifier: str, *, first_lowercase: bool = False) -> str:
+    """
+    Convert a kebab-case identifier to camelCase format.
+
+    Args:
+        identifier (str): A string in kebab-case format.
+        first_lowercase (bool, optional): If set to True, the resulting identifier starts with a lowercase letter.
+            Default is False.
+
+    Returns:
+        str: A string in camelCase format.
+    """
+    return join_camelcase(split_kebabcase(identifier), first_lowercase=first_lowercase)
+
+def kebabcase_to_snakecase(identifier: str, *, preserve_case: bool = False) -> str:
+    """
+    Convert a kebab-case identifier to snake_case format.
+
+    Args:
+        identifier (str): A string in kebab-case format.
+        preserve_case (bool, optional): If set to True, the original case of identifier is preserved.
+            Default is False.
+
+    Returns:
+        str: A string in snake_case format.
+    """
+    return join_snakecase(split_kebabcase(identifier), preserve_case=preserve_case)
+
+def camelcase_to_snakecase(identifier: str, *, preserve_case: bool = False) -> str:
+    """
+    Convert a camelCase identifier to snake_case format.
+
+    Args:
+        identifier (str): A string in camelCase format.
+        preserve_case (bool, optional): If set to True, the original case of identifier is preserved.
+            Default is False.
+
+    Returns:
+        str: A string in snake_case format.
+    """
+    return join_snakecase(split_camelcase(identifier), preserve_case=preserve_case)
+
+def camelcase_to_kebabcase(identifier: str, *, preserve_case: bool = False) -> str:
+    """
+    Convert a camelCase identifier to kebab-case format.
+
+    Args:
+        identifier (str): A string in camelCase format.
+        preserve_case (bool, optional): If set to True, the original case of identifier is preserved.
+            Default is False.
+
+    Returns:
+        str: A string in kebab-case format.
+    """
+    return join_kebabcase(split_camelcase(identifier), preserve_case=preserve_case)
