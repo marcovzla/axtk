3247b996743685285d1164c1406dab0156cd2320
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-09-13 11:13:23 -0700

    config class

---
+++ src/axtk/config.py
@@ -0,0 +1,134 @@
+from __future__ import annotations
+from typing import Any, Union, Optional
+from collections.abc import Mapping, MutableMapping
+import json
+import copy
+
+
+CONFIG_FIELDS_NAME = '_config_fields'
+
+
+class Config(MutableMapping):
+    """A Config class that acts like a dictionary but provides additional features."""
+
+    def __init__(self, *args, **kwargs):
+        self._config_fields: dict[str, Any] = {}
+        for k, v in dict(*args, **kwargs).items():
+            self[k] = v
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self._config_fields})'
+
+    def __len__(self):
+        return len(self._config_fields)
+
+    def __iter__(self):
+        return iter(self._config_fields)
+
+    def __contains__(self, key):
+        return key in self._config_fields
+
+    def __getitem__(self, key):
+        return self._config_fields[key]
+
+    def __setitem__(self, key, value):
+        if isinstance(value, Mapping):
+            value = self.new_config(value)
+        self._config_fields[key] = value
+
+    def __delitem__(self, key):
+        del self._config_fields[key]
+    
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+    def __setattr__(self, name, value):
+        if name == CONFIG_FIELDS_NAME:
+            super().__setattr__(CONFIG_FIELDS_NAME, value)
+        else:
+            self[name] = value
+
+    def __delattr__(self, name):
+        try:
+            del self[name]
+        except KeyError:
+            raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
+
+    def __copy__(self):
+        # Create a new instance without calling __init__
+        new_copy = self.__class__.__new__(self.__class__)
+        # Manually copying _config_fields (shallow copy)
+        new_copy._config_fields = dict(self._config_fields)
+        return new_copy
+
+    def __deepcopy__(self, memo):
+        # Avoiding recursive deep copies
+        if id(self) in memo:
+            return memo[id(self)]
+        # Creating a new instance without calling __init__
+        new_copy = self.__class__.__new__(self.__class__)
+        memo[id(self)] = new_copy
+        # Manually copying _config_fields (deep copy)
+        new_copy._config_fields = copy.deepcopy(self._config_fields, memo)
+        return new_copy
+
+    def __or__(self, other):
+        new_config = copy.deepcopy(self)
+        new_config.update(other)
+        return new_config
+
+    def __ror__(self, other):
+        new_config = self.new_config(copy.deepcopy(other))
+        new_config.update(self)
+        return new_config
+
+    def __ior__(self, other):
+        self.update(other)
+        return self
+
+    def update(self, other):
+        other = self.new_config(other)
+        for key, value in other.items():
+            if key in self and isinstance(self[key], Config) and isinstance(value, Config):
+                self[key].update(value)
+            else:
+                self[key] = value
+
+    def copy(self, *, shallow: bool = False):
+        return copy.copy(self) if shallow else copy.deepcopy(self)
+
+    def new_config(self, *args, **kwargs):
+        return self.__class__(*args, **kwargs)
+
+    @classmethod
+    def from_json(cls, s: str):
+        """Load a Config object from a JSON string."""
+        return json.loads(s, cls=ConfigJSONDecoder, config_cls=cls)
+
+    def to_json(
+            self,
+            *,
+            ensure_ascii: bool = False,
+            indent: Optional[Union[int, str]] = None,
+    ):
+        """Convert the Config object to a JSON string."""
+        return json.dumps(self, ensure_ascii=ensure_ascii, indent=indent, cls=ConfigJSONEncoder)
+
+
+class ConfigJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Config):
+            return dict(obj)
+        return super().default(obj)
+
+
+class ConfigJSONDecoder(json.JSONDecoder):
+    def __init__(self, config_cls=Config, **kwargs):
+        super().__init__(object_hook=self.object_hook, **kwargs)
+        self.config_cls = config_cls
+    
+    def object_hook(self, dct):
+        return self.config_cls(dct)
