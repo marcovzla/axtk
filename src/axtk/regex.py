676266793ed6c4933eeefb4994b8a02a797a9296
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-09-22 17:29:11 -0700

    regex

--- src/axtk/regex.py
+++ src/axtk/regex.py
@@ -2,20 +2,66 @@ import re
 from typing import Optional
 
 
-DECIMAL = re.compile(r'[-+]?(?:0(?:[0_]*0)?|[1-9](?:[0-9_]*[0-9])?)')
-"""Matches a decimal number."""
 
-BINARY = re.compile(r'[-+]?0[bB][01_]*[01]')
-"""Matches a binary number."""
+def delimited_string(
+        delimiters: str,
+        close_delimiters: Optional[str] = None,
+        escape_chars: Optional[str] = '\\',
+        return_string: bool = False,
+) -> str | re.Pattern[str]:
+    """
+    Create a regex pattern to capture strings between matching delimiters.
 
-OCTAL = re.compile(r'[-+]?0[oO][0-7_]*[0-7]')
-"""Matches an octal number."""
+    Parameters:
+        delimiters (str): Opening delimiters.
+        close_delimiters (Optional[str]): Closing delimiters. Defaults to the same as delimiters.
+        escape_chars (Optional[str]): Characters to treat as escape characters.
+        return_string (bool): If True, returns the regex pattern as a string; else, a compiled regex.
 
-HEXADECIMAL = re.compile(r'[-+]?0[xX][a-fA-F0-9_]*[a-fA-F0-9]')
-"""Matches a hexadecimal number."""
+    Returns:
+        str | regex.Pattern: The regex pattern.
+    """
+    
+    # Ensure we have at least one delimiter
+    if not delimiters:
+        raise ValueError('At least one delimiter is required.')
 
-FLOAT = re.compile(r'[-+]?(?:[0-9](?:[0-9_]*[0-9])?)?\.?(?:[0-9](?:[0-9_]*[0-9])?)(?:[eE][-+]?[0-9](?:[0-9_]*[0-9])?)?')
-"""Matches a floating point number."""
+    # Default to using the same delimiter for closing if none is provided
+    close_delimiters = close_delimiters or delimiters
+
+    # Check for matching lengths of delimiters
+    if len(delimiters) != len(close_delimiters):
+        raise ValueError('Mismatch between open and close delimiters.')
+
+    # Duplicate a single escape character for all delimiters if necessary
+    if escape_chars and len(escape_chars) == 1:
+        escape_chars *= len(delimiters)
+
+    # Check for matching lengths of delimiters and escape characters
+    if escape_chars and len(escape_chars) != len(delimiters):
+        raise ValueError('Mismatch between delimiters and escape characters.')
+
+    # Construct patterns
+    patterns = []
+    for i in range(len(delimiters)):
+        open_delim = regex.escape(delimiters[i])
+        close_delim = regex.escape(close_delimiters[i])
+        if escape_chars is None:
+            pattern = f'{open_delim}[^{close_delim}]*{close_delim}'
+        else:
+            esc_char = regex.escape(escape_chars[i])
+            if esc_char == close_delim:
+                pattern = f'{open_delim}[^{close_delim}]*(?:{close_delim}{close_delim}[^{close_delim}]*)*{close_delim}'
+            else:
+                pattern = f'{open_delim}[^{esc_char}{close_delim}]*(?:{esc_char}.[^{esc_char}{close_delim}]*)*{close_delim}'
+        patterns.append(pattern)
+
+    # Combine patterns using a non-capturing group
+    pattern = '(?:' + '|'.join(patterns) + ')'
+
+    # Return the pattern either as a string or as a compiled regex
+    return pattern if return_string else re.compile(pattern)
+
 
 
 def bracketed_string(
@@ -23,6 +69,22 @@ def bracketed_string(
         escape_chars: Optional[str] = '\\',
         return_string: bool = False,
 ) -> str | re.Pattern[str]:
+    """
+    Create a regex pattern to capture strings between matching brackets.
+
+    Parameters:
+        brackets (str): String containing pairs of brackets. e.g., "{}[]".
+        escape_chars (Optional[str]): Characters to treat as escape characters.
+        return_string (bool): If True, returns the regex pattern as a string; else, a compiled regex.
+
+    Returns:
+        str | regex.Pattern: The regex pattern.
+    """
+    
+    # Ensure the provided brackets string contains even number of characters (pairs)
+    if len(brackets) % 2 != 0:
+        raise ValueError('The brackets string should contain pairs of opening and closing brackets.')
+
     return delimited_string(
         delimiters=brackets[::2],
         close_delimiters=brackets[1::2],
@@ -31,57 +93,55 @@ def bracketed_string(
     )
 
 
-def delimited_string(
-        delimiters: str,
-        close_delimiters: Optional[str] = None,
-        escape_chars: Optional[str] = '\\',
+
+def anything_except(
+        *args: str,
+        escape_args: bool = True,
+        allow_spaces: bool = True,
+        allow_newline: bool = False,
+        allow_empty_match: bool = False,
         return_string: bool = False,
 ) -> str | re.Pattern[str]:
+    """
+    Returns a regex pattern that matches any string except the provided arguments.
 
-    # ensure we have delimiters
-    if not delimiters:
-        raise ValueError('at least one delimiter is required')
+    Parameters:
+        *args (str): Strings to exclude.
+        escape_args (bool): If True, escapes regex metacharacters in provided args.
+        allow_spaces (bool): If True, pattern can match spaces.
+        allow_newline (bool): If True, pattern can match newlines.
+        allow_empty_match (bool): If True, pattern allows for an empty match.
+        return_string (bool): If True, returns the regex pattern as a string; else, a compiled regex.
 
-    # if close delimiters were not provided, default to open delimiters
-    if close_delimiters is None:
-        close_delimiters = delimiters
+    Returns:
+        str | regex.Pattern: The regex pattern.
+    """
 
-    # ensure we have a close delimiter for each open delimiter
-    if len(delimiters) != len(close_delimiters):
-        raise ValueError('delimiters and close_delimiters do not match')
+    # Check for invalid combination of arguments
+    if allow_newline and not allow_spaces:
+        raise ValueError("Cannot allow newlines without allowing spaces.")
 
-    # if only one escape character was provided, reuse it for all delimiters
-    if escape_chars is not None and len(escape_chars) == 1:
-        escape_chars *= len(delimiters)
+    # Escape regex metacharacters if needed
+    if escape_args:
+        args = [re.escape(arg) for arg in args]
 
-    # ensure we have an escape character for each delimiter
-    if escape_chars is not None and len(escape_chars) != len(delimiters):
-        raise ValueError('delimiters and escape_chars do not match')
+    # Create negative lookahead for arguments to exclude
+    negative_lookahead = '|'.join(f'(?:{arg})' for arg in args)
 
-    # make pattern for each delimiter
-    patterns = []
-    for i in range(len(delimiters)):
-        delim = re.escape(delimiters[i])
-        cdelim = re.escape(close_delimiters[i])
-        if escape_chars is None:
-            pattern = f'{delim}[^{cdelim}]*{cdelim}'
-        else:
-            esc = re.escape(escape_chars[i])
-            if esc == cdelim:
-                pattern = f'{delim}[^{cdelim}]*(?:{cdelim}{cdelim}[^{cdelim}]*)*{cdelim}'
-            else:
-                pattern = f'{delim}[^{esc}{cdelim}]*(?:{esc}.[^{esc}{cdelim}]*)*{cdelim}'
-        patterns.append(pattern)
-
-    # merge patterns
-    if len(patterns) == 1:
-        pattern = patterns[0]
+    # Determine the character set to match
+    if allow_spaces:
+        char_match = '(?s:.)' if allow_newline else '.'
     else:
-        pattern = '(?:' + '|'.join(patterns) + ')'
+        char_match = r'\S'
 
-    # maybe return string
-    if return_string:
-        return pattern
+    # Construct the pattern using negative lookahead and the char_match
+    pattern = f'(?:(?!{negative_lookahead}){char_match})'
 
-    # return compiled pattern
-    return re.compile(pattern)
+    # Add quantifier based on the allow_empty_match flag
+    pattern += '*' if allow_empty_match else '+'
+
+    # Wrap in non-capturing parenthesis
+    pattern = f'(?:{pattern})'
+
+    # Return the pattern either as a string or as a compiled regex
+    return pattern if return_string else re.compile(pattern)
