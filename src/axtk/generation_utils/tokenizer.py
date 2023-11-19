4ada6694e92b4cc13f1e47ceddb1fd4ddefd1190
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-11-19 11:50:19 -0700

    tokenizer protocol

--- src/axtk/generation_utils/tokenizer.py
+++ src/axtk/generation_utils/tokenizer.py
@@ -1,4 +1,6 @@
-from typing import Protocol, Optional, Union, List
+from typing import Protocol, Optional, Union
+import numpy as np
+import torch
 
 
 class HuggingFaceLikeTokenizer(Protocol):
@@ -10,77 +12,52 @@ class HuggingFaceLikeTokenizer(Protocol):
     converting between tokens and their corresponding IDs.
     """
 
-    @property
-    def vocab_size(self) -> int:
-        pass
+    vocab_size: int
 
-    @property
-    def bos_token(self) -> Optional[str]:
-        pass
+    bos_token: Optional[str] = None
+    eos_token: Optional[str] = None
+    unk_token: Optional[str] = None
+    sep_token: Optional[str] = None
+    pad_token: Optional[str] = None
+    cls_token: Optional[str] = None
+    mask_token: Optional[str] = None
 
-    @property
-    def bos_token_id(self) -> Optional[int]:
-        pass
+    bos_token_id: Optional[int] = None
+    eos_token_id: Optional[int] = None
+    unk_token_id: Optional[int] = None
+    sep_token_id: Optional[int] = None
+    pad_token_id: Optional[int] = None
+    cls_token_id: Optional[int] = None
+    mask_token_id: Optional[int] = None
 
-    @property
-    def eos_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def eos_token_id(self) -> Optional[int]:
-        pass
-
-    @property
-    def unk_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def unk_token_id(self) -> Optional[int]:
-        pass
-
-    @property
-    def sep_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def sep_token_id(self) -> Optional[int]:
-        pass
-
-    @property
-    def pad_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def pad_token_id(self) -> Optional[int]:
-        pass
-
-    @property
-    def cls_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def cls_token_id(self) -> Optional[int]:
-        pass
-
-    @property
-    def mask_token(self) -> Optional[str]:
-        pass
-
-    @property
-    def mask_token_id(self) -> Optional[int]:
-        pass
-
-    def encode(self, text: str, add_special_tokens: bool = True) -> List[int]:
+    def encode(
+            self,
+            text: str,
+            add_special_tokens: bool = True,
+    ) -> list[int]:
         raise NotImplementedError()
 
-    def decode(self, ids: List[int], skip_special_tokens: bool = False) -> str:
+    def decode(
+            self,
+            ids: Union[int, list[int], np.ndarray, torch.Tensor],
+            skip_special_tokens: bool = False,
+    ) -> str:
         raise NotImplementedError()
 
-    def convert_ids_to_tokens(self, ids: Union[int, List[int]]) -> Union[str, List[str]]:
+    def convert_ids_to_tokens(
+            self,
+            ids: Union[int, list[int], np.ndarray, torch.Tensor],
+    ) -> Union[str, list[str]]:
         raise NotImplementedError()
 
-    def convert_tokens_to_ids(self, tokens: Union[str, List[str]]) -> Union[int, List[int]]:
+    def convert_tokens_to_ids(
+            self,
+            tokens: Union[str, list[str]],
+    ) -> Union[int, list[int]]:
         raise NotImplementedError()
 
-    def convert_tokens_to_string(self, tokens: List[str]) -> str:
+    def convert_tokens_to_string(
+            self,
+            tokens: list[str],
+    ) -> str:
         raise NotImplementedError()
