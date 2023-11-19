fb915211489159b9ba2391df099bedc7fd2b60c3
Marco Antonio Valenzuela Escárcega <marcos.vzla@gmail.com>
2023-11-19 11:44:51 -0700

    typevar

--- src/axtk/typing.py
+++ src/axtk/typing.py
@@ -1,5 +1,6 @@
 import os
 from typing import Any, Union, Callable, TypeVar
+from numbers import Number
 
 Args = tuple[Any, ...]
 KwArgs = dict[str, Any]
@@ -8,3 +9,6 @@ PathLike = Union[str, os.PathLike[str]]
 
 F = TypeVar('F', bound=Callable)
 """Type variable bound to Callable."""
+
+N = TypeVar('N', bound=Number)
+"""Type variable bound to Number."""
